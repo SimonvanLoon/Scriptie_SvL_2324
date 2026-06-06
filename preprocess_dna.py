0a2c016e732ee46ac89e56f19d588a38215fef6b
SimonvanLoon <s.p.c.a.van.loon@student.rug.nl>
2026-06-06 22:00:34 +0200

    Update preprocess_dna.py
    Only include annotated articles

--- preprocess_dna.py
+++ preprocess_dna.py
@@ -1,12 +1,23 @@
+# preprocess_dna.py
 import os
 import json
-# preprocess_dna.py
+
+# Load templates (these define which articles are annotated)
+with open("ai_template_dna.json", "r", encoding="utf-8") as f:
+    templates = json.load(f)
+
+annotated_ids = set(str(k) for k in templates.keys())
 
 data = {}
 
 for fname in sorted(os.listdir("Files")):
     if fname.endswith(".txt"):
         file_id = fname.split("_")[-1].replace(".txt", "")
+
+        # Only include articles that have annotations
+        if file_id not in annotated_ids:
+            continue
+
         with open(os.path.join("Files", fname), "r", encoding="utf-8") as f:
             data[file_id] = {
                 "file_id": file_id,
